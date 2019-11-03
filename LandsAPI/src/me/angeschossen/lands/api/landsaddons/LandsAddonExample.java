240372fd718a0f03635239f68d543d097c7bf338
Angeschossen <angeschossenofficial@gmail.com>
2019-11-03 01:09:13 +0100

    Changes

--- LandsAPI/src/me/angeschossen/lands/api/landsaddons/LandsAddonExample.java
+++ LandsAPI/src/me/angeschossen/lands/api/landsaddons/LandsAddonExample.java
@@ -11,7 +11,6 @@ This class is just a example. It's not part of the API.
 public class LandsAddonExample {
 
     private final LandsAddon landsAddon;
-    protected final String key;
 
     private LandsAddonExample(Plugin yourPlugin) {
 
@@ -22,25 +21,6 @@ public class LandsAddonExample {
         access your addon.
          */
         landsAddon = new LandsAddon(yourPlugin, false);
-
-        /*
-        Initialize the addon.
-        You need to key to disable your landsAddon, if you ever want to.
-         */
-        key = landsAddon.initialize();
-    }
-
-    /**
-     * Disable your landsAddon.
-     * For example, if plugin disables.
-     */
-    public void disableLandsAddon() {
-
-        /*
-        Disable landsAddon if you want.
-        You need the received key.
-         */
-        landsAddon.disable(key);
     }
 
     //Just a test
